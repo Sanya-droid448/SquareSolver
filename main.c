420b22f37a5039aed2fbe13f4ecd2dd2d3cdc067
Sanya-droid448 <alexandrserow0504@gmail.com>
2026-07-20 13:42:59 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,68 +1,67 @@
-    #include<stdio.h>
-    #include<math.h>
-    #include <string.h>
+#include<stdio.h>
+#include<math.h>
+#include <string.h>
 
-
-    int main()
-    {
+int main()
+{
     double coef_a = 0, coef_b = 0, coef_c = 0, D = 0;
-    double solut1, solut2;
+    double solut1 = 0, solut2 = 0;
     int choice;
     int numargumread = 0;
-    while (1)
-    {
-    printf("1.Solve a quadratic equation.\n");
-    printf("2. Exit\n") ;
-    printf("\n Your choice: ");
-    scanf("%d", &choice);
-   switch (choice)
-   {
-    case 1:
-    printf("\n Enter the coefficients a, b, and c.:\n");
-   numargumread= scanf("%lf %lf %lf", &coef_a, &coef_b, &coef_c );
-    if (numargumread != 3)
-   {
-    printf("You need to enter three numbers!\n");
-    continue;
-   }
+        while (1)
+        {
+        printf("1.Solve a quadratic equation.\n");
+        printf("2. Exit\n") ;
+        printf("\n Your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+            printf("\n Enter the coefficients a, b, and c.:\n");
+            numargumread= scanf("%lf %lf %lf", &coef_a, &coef_b, &coef_c );
+            if (numargumread != 3)
+        {
+                printf("You need to enter three numbers!\n");
+                continue;
+}
 
 
-   if (coef_a==0)
- {
-    double lineequat = -coef_c / coef_b;
-    printf("x=%lf\n",lineequat);
-    if (coef_c==0)
-    {
-        printf("The equation has no roots.\n");
-    continue;
-    }
-  }
-  D = coef_b * coef_b - 4 * coef_a * coef_c;
-    printf("The discriminant is equal to: %.2lf\n", D);
-    if ( fabs(D) ==0)
-    {
-    solut1 = (-coef_b + sqrt(D)) / ( 2 * coef_a);
-    printf("x1= %.2lf\n", solut1);
+            if (coef_a==0)
+        {
+                    double lineequat = -coef_c / coef_b;
+                    printf("x=%lf\n",lineequat);
+                    if (coef_c==0)
+        {
+                        printf("The equation has no roots.\n");
+                        continue;
+        }
+        }
+        D = coef_b * coef_b - 4 * coef_a * coef_c;
+        printf("The discriminant is equal to: %.2lf\n", D);
+        if ( fabs(D) ==0)
+{
+        solut1 = (-coef_b + sqrt(D)) / ( 2 * coef_a);
+        printf("x1= %.2lf\n", solut1);
 
-    }
+}
 
-    else if ( D>0)
-     {
-     solut1 = (-coef_b + sqrt(D)) / ( 2 * coef_a);
-     solut2=(-coef_b - sqrt(D)) / ( 2 * coef_a);
+        else if ( D>0)
+{
+        solut1 = (-coef_b + sqrt(D)) / ( 2 * coef_a);
+        solut2=(-coef_b - sqrt(D)) / ( 2 * coef_a);
         printf("x1=%.2lf, x2=%.2lf\n", solut1, solut2);
-    }
-      else
-       {
-          printf("There are no real solutions.\n");
-      }
-      break;
+}
+        else
+{
+        printf("There are no real solutions.\n");
+}
+        break;
 
 
 
-   case 2:
-   printf("Bye!\n");
-    return 0;
+        case 2:
+        printf("Bye!\n");
+        return 0;
 
     default:
     printf("Wrong choice!\n");
@@ -70,8 +69,8 @@
 
 
 
-    }
+}
 
- }
+        }
     return 0;
-    }
+}
